abc3c59b4efb314a8bdf248e3aeb99fd6c6eff98
Thomas Speck <thomas.speck@itp4.uni-stuttgart.de>
2023-02-09 09:14:06 +0100

    cosmetic changes

--- restflow/symtools.py
+++ restflow/symtools.py
@@ -1,12 +1,12 @@
-from sympy import *
+import itertools
+import sympy
 
-def Taylor_polynomial_sympy(function_expression, variable_list, evaluation_point, degree):
+def taylor(function_expression, variable_list, evaluation_point, degree):
     """
-    Returns a Sympy expression of the Taylor series up to a given degree, of a given multivariate expression, approximated as a multivariate polynomial evaluated at the evaluation_point
+    Returns a sympy expression of the Taylor series up to a given degree, of
+    a given multivariate expression, approximated as a multivariate
+    polynomial evaluated at the evaluation_point
     """
-    from sympy import factorial, Matrix, prod
-    import itertools
-
     n_var = len(variable_list)
     point_coordinates = [(i, j) for i, j in (zip(variable_list, evaluation_point))]  # list of tuples with variables and their evaluation_point coordinates, to later perform substitution
 
@@ -18,17 +18,20 @@ def Taylor_polynomial_sympy(function_expression, variable_list, evaluation_point
     polynomial = 0
     for i in range(n_terms):
         partial_derivatives_at_point = function_expression.diff(*deriv_orders_as_input[i]).subs(point_coordinates)  # e.g. df/(dx*dy**2)
-        denominator = prod([factorial(j) for j in deriv_orders[i]])  # e.g. (1! * 2!)
-        distances_powered = prod([(Matrix(variable_list) - Matrix(evaluation_point))[j] ** deriv_orders[i][j] for j in range(n_var)])  # e.g. (x-x0)*(y-y0)**2
+        denominator = sympy.prod([sympy.factorial(j) for j in deriv_orders[i]])  # e.g. (1! * 2!)
+        distances_powered = sympy.prod([(sympy.Matrix(variable_list) - sympy.Matrix(evaluation_point))[j] ** deriv_orders[i][j] for j in range(n_var)])  # e.g. (x-x0)*(y-y0)**2
         polynomial += partial_derivatives_at_point / denominator * distances_powered
     return polynomial
 
 def all_coeffs(expr,variables):
-    # Returns a dictionary of all monomials of multivariate polynomials. (Extends the sympy function all_coeffs() to multivariate polynomials)
-    x = IndexedBase('x')
+    """
+    Returns a dictionary of all monomials of multivariate polynomials.
+    (Extends the sympy function all_coeffs() to multivariate polynomials)
+    """
+    x = sympy.IndexedBase('x')
     expr = expr.expand()
     free = list(variables)
-    pows = [p.as_base_exp() for p in expr.atoms(Pow,Symbol)]
+    pows = [p.as_base_exp() for p in expr.atoms(sympy.Pow,sympy.Symbol)]
     P = {}
     for p,e in pows:
         if p not in free:
@@ -40,13 +43,30 @@ def all_coeffs(expr,variables):
     reps = dict([(f, x[i]) for i,f in enumerate(free)])
     xzero = dict([(v,0) for k,v in reps.items()])
     e = expr.xreplace(reps); reps = {v:k for k,v in reps.items()}
-    return dict([(m.xreplace(reps), e.coeff(m).xreplace(xzero) if m!=1 else e.xreplace(xzero)) for m in monoms(*[P[f] for f in free])])
+    return dict([(m.xreplace(reps), e.coeff(m).xreplace(xzero) if m!=1 else e.xreplace(xzero)) for m in _monoms(*[P[f] for f in free])])
 
-def monoms(*o):
+def _monoms(*o):
     # Used for all_coeffs
-    x = IndexedBase('x')
+    x = sympy.IndexedBase('x')
     f = []
     for i,o in enumerate(o):
-        f.append(Poly([1]*(o+1),x[i]).as_expr())
-    return Mul(*f).expand().args
-    
+        f.append(sympy.Poly([1]*(o+1),x[i]).as_expr())
+    return sympy.Mul(*f).expand().args
+
+def func_coeff(polynomial, q, p):
+    """
+    Returns a dictionary of the monomials of the integrals
+    """    
+    # prepare
+    cs_psi = sympy.symbols('cos_psi')
+    polynomial = sympy.Poly(polynomial,(q.sym,p.sym,cs_psi)).as_expr()
+    variables = [q.sym,p.sym,cs_psi]
+    var_temp = []
+    # following for loop removes the dependency of the polynomial on q,p or
+    # cosψ if these monomials do not exist e.g. q^2+q has no p monomial
+    for var in variables:
+        if var not in polynomial.free_symbols:
+            var_temp.append(var)
+    variables = [element for element in variables if element not in var_temp]
+    coeffs = all_coeffs(polynomial,variables)
+    return coeffs
